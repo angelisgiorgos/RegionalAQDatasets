f48b6a5de7d8b56b7fe5dd028ed616cf08d6f19b
Giorgos Angelis <angelisg@iti.gr>
2024-05-30 10:54:46 +0300

    data provider

---
+++ src/data_provider/data_factory.py
@@ -0,0 +1,39 @@
+from torch.utils.data import DataLoader
+from src.data_provider.dataloader import Dataset_AirQuality
+
+data_dict = {
+    'air': Dataset_AirQuality,
+}
+
+
+def data_provider(args, flag):
+    Data = data_dict[args.data]
+    timeenc = 0 if args.embed != 'timeF' else 1
+
+    shuffle_flag = False if flag == 'test' else True
+    drop_last = True
+    batch_size = args.batch_size
+    freq = args.freq
+
+    data_set = Data(
+        args=args,
+        root_path=args.root_path,
+        data_path=args.data_path,
+        flag=flag,
+        size=[args.seq_len, args.label_len, args.pred_len],
+        features=args.features,
+        target=args.target,
+        station_name=args.station_name,
+        timeenc=timeenc,
+        freq=freq,
+        seasonal_patterns=args.seasonal_patterns
+        )
+    print(flag, len(data_set))
+    data_loader = DataLoader(
+        data_set,
+        batch_size=batch_size,
+        shuffle=shuffle_flag,
+        num_workers=args.num_workers,
+        drop_last=drop_last)
+
+    return data_set, data_loader
